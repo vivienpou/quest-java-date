d09affe17d0bb6285b93c071ab4d3687bc88e5cb
bastienwcs <bastien@wildcodeschool.fr>
2019-03-28 17:59:46 +0100

    init

---
+++ TestDate.java
@@ -0,0 +1,23 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
+import java.util.Scanner; 
+
+class TestDate {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please fill a day (1-31) :");
+        int day = sc.nextInt();
+        System.out.println("Please fill a month (1-12) :");
+        int month = sc.nextInt();
+        System.out.println("Please fill a year :");
+        int year = sc.nextInt();
+        sc.close();
+
+        // TODO : your code after this line
+        
+    }
+}
